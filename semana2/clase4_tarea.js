e3a34720f8e3ff5e20408b0be1ff68d4d3b23ec0
Martin <martin-spark117@hotmail.com>
2025-05-20 20:25:29 -0600

    Implementar función para analizar calificaciones y agregar información de resultados

--- semana2/clase4_tarea.js
+++ semana2/clase4_tarea.js
@@ -12,3 +12,37 @@
 //Promedio general
 //
 //La calificación más alta y la más baja
+
+const { ask } = require('../helpers/input');
+
+function analizarCalificaciones(calificacion) {
+  let total = 0;
+  let alumnos = calificacion.length;
+  let aprobados = 0;
+  let reprobados = 0;
+
+  for (let i = 0; i < calificacion.length; i++) {
+    total = total + calificacion[i];
+     if(calificacion[i]>=70){aprobados++}
+     if(calificacion[i]<70){reprobados++}
+  }
+ 
+  const promedio = total / alumnos;
+
+  return {promedio, alumnos, aprobados, reprobados}
+}
+
+async function main() {
+
+const calificacion=[59, 99, 48, 56, 43, 95, 43, 58, 76, 76, 69, 56];
+
+const resultado = analizarCalificaciones(calificacion);
+
+
+console.log(`La cantidad de alumnos es de = ${resultado.alumnos}`)
+console.log(`Promedio = ${resultado.promedio}`)
+console.log(`Aprobados = ${resultado.aprobados}`)
+console.log(`Reprobados = ${resultado.reprobados}`)
+}
+
+main();
---
+++ semana2/ejercicio1s3.js
@@ -0,0 +1,18 @@
+const { ask } = require('../helpers/input');
+
+async function main() {
+
+const alumno = {
+  nombre:"",
+  matricula:'',
+  primedio: 75,
+  edad: 19,
+  mostrarInfo: function(){
+    if (alumno.edad >= 18);
+}
+}
+console.log(`El alumno puede votar${this.edad}`)
+main();
+
+
+}
